f05611d49435ab52b0664c094bc9ea46ffb6f7c2
10012 <nas7062@naver.com>
2024-11-19 19:29:21 +0900

    feat(EventBanner) : 이벤트 배너 생성
    EventData를 fetch로 받아와 events에 저장 후
    각 이벤트 div 에 hover시 해당 이벤트를 selected 하게 하여
    해당 이벤트를 보여주게 하고 selected된 이벤트를 border 색상을
    변하게하여 변화를 주어 표시하게함

---
+++ src/Components/EventBanner.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+
+const EventBanner = () => {
+    const [selected, setSelected] = useState(0);
+    const [events, setEvents] = useState([]);
+    const fetchEvents = async () => {
+        try {
+            const response = await fetch('/data/Event.json');
+            const data = await response.json();
+            setEvents(data);
+
+        } catch (error) {
+            console.error("Error fetching events:", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchEvents();
+    }, []);
+    console.log(selected, events);
+    const SelectedEvent = (index) => {
+        setSelected(index);
+    }
+    return (
+        <div className="mx-auto max-w-screen-md mt-20 h-96">
+            <div className="flex justify-between items-center">
+                <h2 className="text-3xl font-bold ">이벤트</h2>
+                <button className="px-2 py-1 bg-black text-white rounded mt-1 mr-1">전체보기 &gt;</button>
+            </div>
+            <div>
+                {events.length > 0 && selected >= 0 && (
+                    <div className="rounded-lg p-4 h-96 w-4/6 float-left ">
+                        <img
+                            className="w-full h-72  rounded-lg mt-2"
+                            src={events[selected].image}
+                            alt={events[selected].title}
+                        />
+                        <div className="text-center">
+                            <h3 className="text-lg font-bold ">{events[selected].title}</h3>
+                            {events[selected].tag.map((tag, index) => (
+                                <span className="border rounded-lg bg-gray-300 mr-3" key={index}>#{tag}</span>
+                            ))}
+                        </div>
+                    </div>
+                )}
+                <div className="float-right mt-4 w-64 ">
+                    {events.map((event) => (
+                        <div key={event.id}>
+                            <div className={`mt-3 flex flex-col justify-between border text-center rounded-lg cursor-pointer ${selected === event.id - 1 ? "border-black" : "border-gray-300"
+                                }`} onMouseEnter={() => SelectedEvent(event.id - 1)}>
+                                <h3 >{event.title}</h3>
+                                <div>
+                                    {event.tag.map((tag, index) => (
+                                        <span className="border rounded-lg bg-gray-300" key={index}>#{tag}</span>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+export default EventBanner;
