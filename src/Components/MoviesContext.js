71584d868c9322ab8ef4da5aa8323c08b44d1609
10012 <nas7062@naver.com>
2024-11-21 21:13:07 +0900

    feat(AllMoive) : AllMovie 추가
    AllMoive 추가 및 fetchAllMovies 추가함

--- src/Components/MoviesContext.js
+++ src/Components/MoviesContext.js
@@ -5,6 +5,8 @@ const MoviesContext = createContext();
 
 export function MoviesProvider({ children }) {
   const [movies, setMovies] = useState([]);
+  const [Allmovies, setAllMovies] = useState([]);
+  const [region, setregion] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -27,9 +29,37 @@ export function MoviesProvider({ children }) {
     };
     fetchMovies();
   }, []);
+  useEffect(() => {
+    const fetchAllMovies = async () => {
+      try {
+        const apiKey = process.env.REACT_APP_MOVIE_API_KEY;
+        const totalPages = 5;
+        const requests = []; // 여러 페이지의 데이터를 요청 배열에 추가
+        for (let page = 1; page <= totalPages; page++) {
+          requests.push(
+            axios.get(
+              `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=ko&page=${page}&region=KR`
+            )
+          );
+        }
+        const responses = await Promise.all(requests);
+        const allMovies = responses.flatMap(response => response.data.results);
+        const sortedMovies = allMovies.sort((a, b) => b.vote_count - a.vote_count);
+
+        setAllMovies(sortedMovies);
+        setLoading(false);
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
+      }
+    };
+
+    fetchAllMovies();
+  }, []);
+
 
   return (
-    <MoviesContext.Provider value={{ movies, loading, error }}>
+    <MoviesContext.Provider value={{ Allmovies, movies, loading, error }}>
       {children}
     </MoviesContext.Provider>
   );
