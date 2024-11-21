059b1cb2f9dcffd7cfbd6d272fd35b326fe97b06
10012 <nas7062@naver.com>
2024-11-21 21:11:22 +0900

    feat(router) : react-router-dom 활용
    detail 페이지와 ticket 페이지 설정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import { MoviesProvider } from "./Components/MoviesContext";
 import Main from "./Pages/Main";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import MovieDetail from "./Pages/MovieDetail";
+import Ticket from "./Pages/Ticket";
 function App() {
 
   return (
     <div>
-      <MoviesProvider>
-        <Main />
-      </MoviesProvider>
+      <BrowserRouter>
+        <MoviesProvider>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/detail/:id" element={<MovieDetail />} />
+            <Route path="/ticket" element={<Ticket />} />
+          </Routes>
+        </MoviesProvider>
+      </BrowserRouter>
     </div>
   );
 }
