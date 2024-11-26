8635e7f781ea2c020a284f3d8e1d9b0e61858c77
10012 <nas7062@naver.com>
2024-11-26 12:12:43 +0900

    feat(movieContext) : 무비 데이터를 받아오는 함수
    무비 데이터를 tmdb api로 받아온 후 파이어 스토어에 저장하여
    실시간으로 데이터 처리 및 관리를 할 수 있게 하는 함수

--- src/Components/MoviesContext.js
+++ src/Components/MoviesContext.js
@@ -1,15 +1,33 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
 import axios from "axios";
-
+import { db, collection, addDoc, getDocs } from "../firebase"
 const MoviesContext = createContext();
 
 export function MoviesProvider({ children }) {
   const [movies, setMovies] = useState([]);
-  const [Allmovies, setAllMovies] = useState([]);
-  const [region, setregion] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
+  console.log(movies);
+  const saveMovieToFirestore = async (movie, schedules) => {
+    try {
+      const moviesSnapshot = await getDocs(collection(db, "movies"));
+      const existingMovies = moviesSnapshot.docs.map((doc) => doc.data());
+      const duplicateMovie = existingMovies.find((existingMovie) => existingMovie.title === movie.title);
+      if (!duplicateMovie) {
+        await addDoc(collection(db, "movies"), {
+          title: movie.title,
+          release_date: movie.release_date,
+          overview: movie.overview,
+          poster_path: movie.poster_path,
+          vote_average: movie.vote_average,
+          availableSeats: 100,
+          schedules
+        });
+      }
+    } catch (error) {
+      console.error("Error saving movie:", error);
+    }
+  };
   useEffect(() => {
     const fetchMovies = async () => {
       try {
@@ -21,6 +39,12 @@ export function MoviesProvider({ children }) {
           (a, b) => b.vote_count - a.vote_count
         );
         setMovies(sortedMovies);
+        const responseSchedule = await fetch('/data/Movie.json');
+        const scheduleData = await responseSchedule.json();
+        const schedules = scheduleData.movies; // schedules 로컬 변수화
+        for (const movie of sortedMovies) {
+          await saveMovieToFirestore(movie, schedules);
+        }
         setLoading(false);
       } catch (error) {
         setError(error.message);
@@ -29,37 +53,11 @@ export function MoviesProvider({ children }) {
     };
     fetchMovies();
   }, []);
-  useEffect(() => {
-    const fetchAllMovies = async () => {
-      try {
-        const apiKey = process.env.REACT_APP_MOVIE_API_KEY;
-        const totalPages = 5;
-        const requests = []; // 여러 페이지의 데이터를 요청 배열에 추가
-        for (let page = 1; page <= totalPages; page++) {
-          requests.push(
-            axios.get(
-              `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=ko&page=${page}&region=KR`
-            )
-          );
-        }
-        const responses = await Promise.all(requests);
-        const allMovies = responses.flatMap(response => response.data.results);
-        const sortedMovies = allMovies.sort((a, b) => b.vote_count - a.vote_count);
 
-        setAllMovies(sortedMovies);
-        setLoading(false);
-      } catch (error) {
-        setError(error.message);
-        setLoading(false);
-      }
-    };
-
-    fetchAllMovies();
-  }, []);
 
 
   return (
-    <MoviesContext.Provider value={{ Allmovies, movies, loading, error }}>
+    <MoviesContext.Provider value={{ movies, loading, error }}>
       {children}
     </MoviesContext.Provider>
   );
