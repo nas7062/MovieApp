a019ac28094e439c490c481ee6a763e543bebc9f
10012 <nas7062@naver.com>
2024-12-06 21:36:51 +0900

    api 수정

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, addDoc, getDocs } from 'firebase/firestore';
 import { getAuth } from "firebase/auth"
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    apiKey: "AIzaSyCW4dAnTwvaTnpCWyWFv1faVF3kF_squAg",
     authDomain: "movie-80bbb.firebaseapp.com",
     projectId: "movie-80bbb",
     storageBucket: "movie-80bbb.firebasestorage.app",
